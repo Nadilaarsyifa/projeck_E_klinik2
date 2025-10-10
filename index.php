76d3678d69c5cfc7c92f2031abb7018ed9120b03
nadilaarsyifa <safridahanum27@gmail.com>
2025-10-11 06:18:43 +0700

    komit perbaiki beranda

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
+<?php
 
-            <?php
-            if (isset($_GET['x']) && $_GET['x'] == 'beranda') {
-                $page = "beranda.php";
-                include "main.php";
-            }elseif (isset($_GET['x']) && $_GET['x'] == 'konsultasi') {
-                $page = "konsultasi.php";
-                include "main.php";
-            } elseif (isset($_GET['x']) && $_GET['x'] == 'riwayatkonsultasi') {
-                $page = "riwayatkonsultasi.php";
-                include "main.php";
-            }elseif (isset($_GET['x']) && $_GET['x'] == 'datamedis') {
-                $page = "datamedis.php";
-                include "main.php";
-            }else if (isset($_GET['x']) && $_GET['x'] == 'konsultasimasuk') {
-                $page = "konsultasimasuk.php";
-                include "main.php";
-            }elseif (isset($_GET['x']) && $_GET['x'] == 'user') {
-                $page = "user.php";
-                include "main.php";
-            }elseif (isset($_GET['x']) && $_GET['x'] == 'report') {
-                $page = "report.php";
-                include "main.php";
-            } elseif (isset($_GET['x']) && $_GET['x'] == 'login') {
-                include "login.php";
-            }else {
-                include "main.php";
-            }
-            ?>
+if (isset($_GET['x']) && $_GET['x'] == 'beranda') {
+    $page = "beranda.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'konsultasi') {
+    $page = "konsultasi.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'riwayatkonsultasi') {
+    $page = "riwayatkonsultasi.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'datamedis') {
+    $page = "datamedis.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'konsultasimasuk') {
+    $page = "konsultasimasuk.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'user') {
+    $page = "user.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'report') {
+    $page = "report.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'login') {
+    include "login.php";
+} else {
+    $page = "beranda.php";
+    include "main.php";
+}
+?>
+
            
