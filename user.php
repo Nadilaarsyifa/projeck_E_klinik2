17e9c7b59a965be66abd8f094b1d0734abf94ef2
NilamCahayaSafitri <nilamcahaya012022@gmail.com>
2025-10-27 08:06:47 +0700

    update user

--- user.php
+++ user.php
@@ -26,7 +26,41 @@ while ($record = mysqli_fetch_array($query)) {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <form action="">
+                                        <div class="form-floating mb-3">
+                                            <input type="number" class="form-control" id="floatingInput" placeholder="Username">
+                                            <label for="floatingInput">Username</label>
+                                        </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="floatingInput" placeholder="Your Name">
+                                        <label for="floatingInput">Nama</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-floating mb-3">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected hidden>Pilih Role User</option>
+                                        <option value="1">Admin</option>
+                                        <option value="2">Petugas Klinik</option>
+                                        <option value="3">Mahasiswa</option>
+                                    </select>
+                                    <label for="floatingInput">Role User</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control" id="floatingInput" placeholder="Password" disabled value="12345">
+                                        <label for="floatingPassword">Password</label>
+                                    </div>
+                                </div>
+                            </div>
+                            </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -58,56 +92,55 @@ while ($record = mysqli_fetch_array($query)) {
             <!-- Akhir Modal View -->
 
             <?php
-            if(empty($result)) {
+            if (empty($result)) {
                 echo "Data user tidak ada";
-            }else{
+            } else {
 
             ?>
 
-            <div class="table=responsive">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Role</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $no =1;
-                        foreach ($result as $row) {
-                        ?>
+                <div class="table=responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Role</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            foreach ($result as $row) {
+                            ?>
 
-                        <tr>
-                            <th scope="row"><?php echo $no++?></th>
-                            <td>
-                                <?php echo $row['username'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['nama'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['role'] ?>
-                            </td>
-                            <td class="d-flex">
-                                <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal" data-bs-target="#ModalView"><i class="bi bi-eye"></i></button>
-                                <button class="btn btn-warning btn-sm me-1"><i class="bi bi-pencil-square"></i></button>
-                                <button class="btn btn-danger btn-sm me-1"><i class="bi bi-trash"></i></button>
-                            </td>
-                        </tr>
-                        <?php 
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+                                <tr>
+                                    <th scope="row"><?php echo $no++ ?></th>
+                                    <td>
+                                        <?php echo $row['username'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['nama'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['role'] ?>
+                                    </td>
+                                    <td class="d-flex">
+                                        <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal" data-bs-target="#ModalView"><i class="bi bi-eye"></i></button>
+                                        <button class="btn btn-warning btn-sm me-1"><i class="bi bi-pencil-square"></i></button>
+                                        <button class="btn btn-danger btn-sm me-1"><i class="bi bi-trash"></i></button>
+                                    </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             <?php
             }
             ?>
         </div>
     </div>
 </div>
-
